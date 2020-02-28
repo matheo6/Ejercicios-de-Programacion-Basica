242e2d7e7382d2752367c2429460ddf6b5b8267d
matheo6 <46351733+matheo6@users.noreply.github.com>
2020-02-27 23:15:47 -0500

    Add files via upload

---
+++ Programacion Basica Py/maincalculadoraHex.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 21:22:11 2020
+
+@author: Administrador
+"""
+
+import modulohex
+a=input("ingrese el primer numero")
+b=input("ingrese el segundo numero")
+modulohex.sumahex(a,b)
+modulohex.restahex(a,b)
+modulohex.multhex(a,b)
+modulohex.divehex(a,b)
+
---
+++ Programacion Basica Py/maincalculadorabin.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 21:22:11 2020
+
+@author: Administrador
+"""
+
+import moduloCalculadora
+a=int(input("ingrese el primer numero"))
+b=int(input("ingrese el segundo numero"))
+moduloCalculadora.suma(a,b)
+moduloCalculadora.resta(a,b)
+moduloCalculadora.multiplicion(a,b)
+moduloCalculadora.divicion(a,b)
+moduloCalculadora.potencia(a,b)
+
